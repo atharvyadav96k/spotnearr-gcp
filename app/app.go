3ef75077b0b253292887ce8f4aeb4463332cfbd5
Atharv Yadv <atharvyadav96k@gmail.com>
2026-05-25 15:11:41 +0530

    feat: implement database initialization and migration using GORM

--- app/app.go
+++ app/app.go
@@ -1,19 +1,32 @@
 package app
 
 import (
+	"log"
 	"os"
 
-	db "github.com/atharvyadav96k/spotnearr-gcp/app/database"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
-func Close() {
-	db.Close()
-}
+var DB *gorm.DB
 
-func (a *App) InitDatabase() error {
-	if err := db.Connect(os.Getenv("DATABASE_URL")); err != nil {
-		return err
+func InitDB() {
+	dsn := os.Getenv("DATABASE_URL")
+	if dsn == "" {
+		log.Fatal("DATABASE_URL environment variable is missing")
 	}
-	a.db = db.Get()
-	return nil
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatalf("Failed to connect to database via GORM: %v", err)
+	}
+
+	log.Println("Database connection successfully established.")
+	err = DB.AutoMigrate()
+	if err != nil {
+		log.Fatalf("Failed to run AutoMigration: %v", err)
+	}
+
+	log.Println("Database migration completed successfully.")
 }
